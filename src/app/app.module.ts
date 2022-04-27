bbf79fc07cddb5d139f31b4588cb6f4704a579f0
Jeisonfabfor <fabian00sandra@hotmail.com>
2022-04-27 07:25:43 -0500

    Creacion de modulos

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroModule } from './heroes/heroes.module';
+import {ContadorModule } from './contador/contador.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HeroModule,
+    ContadorModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
